83e4382a8a35dd0e7b466ca6cdea9d5eadb848ed
jaimin1618 <jaimin.chokhawala@gmail.com>
2023-06-25 01:05:22 +0530

    found bug when overriding methods of parent contract, and solved them by commenting overriden methods

--- contracts/SiddhiCred.sol
+++ contracts/SiddhiCred.sol
@@ -6,8 +6,8 @@ import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 import "@openzeppelin/contracts/access/AccessControl.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
 
-contract SiddhiApp is ERC721, ERC721URIStorage, AccessControl {
-    address admin;
+contract SiddhiCred is ERC721, ERC721URIStorage, AccessControl {
+    address public admin;
     mapping(address => string) aboutIssuer;
     using Counters for Counters.Counter;
 
@@ -15,8 +15,8 @@ contract SiddhiApp is ERC721, ERC721URIStorage, AccessControl {
     Counters.Counter private _tokenIdCounter;
 
     constructor() ERC721("Siddhi Credential Soulbound NFT", "SCSBNFT") {
-        admin = msg.sender;
-        _setupRole(DEFAULT_ADMIN_ROLE, admin);
+        _grantRole(DEFAULT_ADMIN_ROLE, msg.sender);
+        _grantRole(ISSUER_ROLE, msg.sender);
     }
 
     /*============================================
@@ -58,10 +58,9 @@ contract SiddhiApp is ERC721, ERC721URIStorage, AccessControl {
         emit IssuerCreated(issuerWalletAddress, contentHash);
     }
 
-    function removeIssuer(address issuerWalletAddress)
-        external
-        onlyRole(DEFAULT_ADMIN_ROLE)
-    {
+    function removeIssuer(
+        address issuerWalletAddress
+    ) external onlyRole(DEFAULT_ADMIN_ROLE) {
         if (!hasRole(ISSUER_ROLE, issuerWalletAddress))
             revert IssuerNotFound(issuerWalletAddress);
 
@@ -82,13 +81,13 @@ contract SiddhiApp is ERC721, ERC721URIStorage, AccessControl {
         emit IssuerUpdated(issuerWalletAddress, contentHash);
     }
 
-    /*============================================
-    ISSUER_ROLE methods | Create/Mint/Issue Certificate | Burn/Revoke Certificate
-    ============================================*/
-    function issueCertificate(address to, string memory contentHash)
-        external
-        onlyRole(ISSUER_ROLE)
-    {
+    // /*============================================
+    // ISSUER_ROLE methods | Create/Mint/Issue Certificate | Burn/Revoke Certificate
+    // ============================================*/
+    function issueCertificate(
+        address to,
+        string memory contentHash
+    ) external onlyRole(ISSUER_ROLE) {
         uint256 tokenId = _tokenIdCounter.current();
         _tokenIdCounter.increment();
         _safeMint(to, tokenId);
@@ -96,10 +95,9 @@ contract SiddhiApp is ERC721, ERC721URIStorage, AccessControl {
         emit CertificateIssued(msg.sender, to, tokenId);
     }
 
-    function _burn(uint256 tokenId)
-        internal
-        override(ERC721, ERC721URIStorage)
-    {
+    function _burn(
+        uint256 tokenId
+    ) internal override(ERC721, ERC721URIStorage) {
         super._burn(tokenId);
     }
 
@@ -110,7 +108,9 @@ contract SiddhiApp is ERC721, ERC721URIStorage, AccessControl {
         _burn(tokenId);
     }
 
-    function supportsInterface(bytes4 interfaceId)
+    function supportsInterface(
+        bytes4 interfaceId
+    )
         public
         view
         override(ERC721, ERC721URIStorage, AccessControl)
@@ -143,344 +143,100 @@ contract SiddhiApp is ERC721, ERC721URIStorage, AccessControl {
     /*============================================
     Other PUBLIC methods
     ============================================*/
-    function checkIsIssuerRole(address issuerWalletAddress)
-        external
-        view
-        returns (bool isIssuer)
-    {
-        return hasRole(ISSUER_ROLE, issuerWalletAddress);
-    }
-
-    function tokenURI(uint256 tokenId)
-        public
-        view
-        override(ERC721, ERC721URIStorage)
-        returns (string memory)
-    {
+    function tokenURI(
+        uint256 tokenId
+    ) public view override(ERC721, ERC721URIStorage) returns (string memory) {
         return super.tokenURI(tokenId);
     }
 
-    function getIssuerInfo(address issuerWalletAddress)
-        external
-        view
-        returns (string memory contentHash)
-    {
+    function getIssuerInfo(
+        address issuerWalletAddress
+    ) external view returns (string memory contentHash) {
         if (!hasRole(ISSUER_ROLE, issuerWalletAddress))
             revert IssuerNotFound(issuerWalletAddress);
         return aboutIssuer[issuerWalletAddress];
     }
 
-    /*============================================
-    Overriding unnecessary inherited methods
-    ============================================*/
-    function approve(address to, uint256 tokenId)
-        public
-        virtual
-        override(ERC721, IERC721)
-    {}
+    function getWalletAddressRole(
+        address walletAddress
+    ) external view returns (string memory role) {
+        if (hasRole(DEFAULT_ADMIN_ROLE, walletAddress)) {
+            return "ADMIN";
+        } else if (hasRole(ISSUER_ROLE, walletAddress)) {
+            return "ISSUER";
+        }
 
-    function grantRole(bytes32 role, address account) public override {}
-
-    function revokeRole(bytes32 role, address account) public override {}
-
-    function renounceRole(bytes32 role, address account) public override {}
-
-    function safeTransferFrom(
-        address from,
-        address to,
-        uint256 tokenId
-    ) public virtual override(ERC721, IERC721) {}
-
-    function safeTransferFrom(
-        address from,
-        address to,
-        uint256 tokenId,
-        bytes memory data
-    ) public virtual override(ERC721, IERC721) {}
-
-    function setApprovalForAll(address operator, bool approved)
-        public
-        override(ERC721, IERC721)
-    {}
-
-    function transferFrom(
-        address from,
-        address to,
-        uint256 tokenId
-    ) public virtual override(ERC721, IERC721) {}
-
-    function getApproved(uint256 tokenId)
-        public
-        view
-        virtual
-        override(ERC721, IERC721)
-        returns (address)
-    {}
-
-    function getRoleAdmin(bytes32 role)
-        public
-        view
-        virtual
-        override
-        returns (bytes32)
-    {}
-
-    function hasRole(bytes32 role, address account)
-        public
-        view
-        virtual
-        override
-        returns (bool)
-    {}
-
-    function isApprovedForAll(address owner, address operator)
-        public
-        view
-        virtual
-        override(ERC721, IERC721)
-        returns (bool)
-    {}
-}
-// SPDX-License-Identifier: MIT
-pragma solidity ^0.8.9;
-
-import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
-import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
-import "@openzeppelin/contracts/access/AccessControl.sol";
-import "@openzeppelin/contracts/utils/Counters.sol";
-
-contract SiddhiApp is ERC721, ERC721URIStorage, AccessControl {
-    address admin;
-    mapping(address => string) aboutIssuer;
-    using Counters for Counters.Counter;
-
-    bytes32 public constant ISSUER_ROLE = keccak256("ISSUER");
-    Counters.Counter private _tokenIdCounter;
-
-    constructor() ERC721("Siddhi Credential Soulbound NFT", "SCSBNFT") {
-        admin = msg.sender;
-        _setupRole(DEFAULT_ADMIN_ROLE, admin);
-    }
-
-    /*============================================
-    Application events
-    ============================================*/
-    event IssuerCreated(address issuerWalletAddress, string issuerContentHash);
-    event IssuerRemoved(address issuerWalletAddress, string issuerContentHash);
-    event IssuerUpdated(address issuerWalletAddress, string issuerContentHash);
-    event CertificateIssued(
-        address issuerWalletAddress,
-        address recipient,
-        uint256 tokenId
-    );
-    event CertificateRevoked(
-        address issuerWalletAddress,
-        address owner,
-        uint256 tokenId
-    );
-
-    /*============================================
-    Application errors
-    ============================================*/
-    error IssuerRoleAlreadyAssigned(address issuerWalletAddress);
-    error IssuerNotFound(address issuerWalletAddress);
-    error TokenNotFound(uint256 tokenId);
-
-    /*============================================
-    ADMIN_ROLE method | manage issuer
-    ============================================*/
-    function createIssuer(
-        address issuerWalletAddress,
-        string memory contentHash
-    ) external onlyRole(DEFAULT_ADMIN_ROLE) {
-        if (hasRole(ISSUER_ROLE, issuerWalletAddress))
-            revert IssuerRoleAlreadyAssigned(issuerWalletAddress);
-
-        aboutIssuer[issuerWalletAddress] = contentHash;
-        _grantRole(ISSUER_ROLE, issuerWalletAddress);
-        emit IssuerCreated(issuerWalletAddress, contentHash);
-    }
-
-    function removeIssuer(address issuerWalletAddress)
-        external
-        onlyRole(DEFAULT_ADMIN_ROLE)
-    {
-        if (!hasRole(ISSUER_ROLE, issuerWalletAddress))
-            revert IssuerNotFound(issuerWalletAddress);
-
-        string memory contentHash = aboutIssuer[issuerWalletAddress];
-        _revokeRole(ISSUER_ROLE, issuerWalletAddress);
-        emit IssuerRemoved(issuerWalletAddress, contentHash);
-        delete aboutIssuer[issuerWalletAddress];
-    }
-
-    function updateIssuer(
-        address issuerWalletAddress,
-        string memory contentHash
-    ) external onlyRole(DEFAULT_ADMIN_ROLE) {
-        if (!hasRole(ISSUER_ROLE, issuerWalletAddress))
-            revert IssuerNotFound(issuerWalletAddress);
-
-        aboutIssuer[issuerWalletAddress] = contentHash;
-        emit IssuerUpdated(issuerWalletAddress, contentHash);
-    }
-
-    /*============================================
-    ISSUER_ROLE methods | Create/Mint/Issue Certificate | Burn/Revoke Certificate
-    ============================================*/
-    function issueCertificate(address to, string memory contentHash)
-        external
-        onlyRole(ISSUER_ROLE)
-    {
-        uint256 tokenId = _tokenIdCounter.current();
-        _tokenIdCounter.increment();
-        _safeMint(to, tokenId);
-        _setTokenURI(tokenId, contentHash);
-        emit CertificateIssued(msg.sender, to, tokenId);
-    }
-
-    function _burn(uint256 tokenId)
-        internal
-        override(ERC721, ERC721URIStorage)
-    {
-        super._burn(tokenId);
-    }
-
-    function burnCertificate(uint256 tokenId) external onlyRole(ISSUER_ROLE) {
-        address owner = ownerOf(tokenId);
-        if (owner == address(0)) revert TokenNotFound(tokenId);
-        emit CertificateRevoked(msg.sender, owner, tokenId);
-        _burn(tokenId);
-    }
-
-    function supportsInterface(bytes4 interfaceId)
-        public
-        view
-        override(ERC721, ERC721URIStorage, AccessControl)
-        returns (bool)
-    {
-        return super.supportsInterface(interfaceId);
-    }
-
-    /*============================================
-    Things to keep in mind for Overriding _beforeTokenTransfer
-    - When from and to are both non-zero, from's tokens will be transferred to to.
-    - When from is zero, the tokens will be minted for to => We are using this condition for creating "Soulbound / Mint Only Tokens"
-    - When to is zero, from's tokens will be burned.
-    - from and to are never both zero.
-    - *batchSize is non-zero.
-    ============================================*/
-    function _beforeTokenTransfer(
-        address from,
-        address to,
-        uint256 tokenId,
-        uint256 batchSize
-    ) internal virtual override {
-        require(
-            from == address(0),
-            "Soulbound tokens cannot be transferred, Token transfer is BLOCKED"
-        );
-        super._beforeTokenTransfer(from, to, tokenId, batchSize);
-    }
-
-    /*============================================
-    Other PUBLIC methods
-    ============================================*/
-    function checkIsIssuerRole(address issuerWalletAddress)
-        external
-        view
-        returns (bool isIssuer)
-    {
-        return hasRole(ISSUER_ROLE, issuerWalletAddress);
-    }
-
-    function tokenURI(uint256 tokenId)
-        public
-        view
-        override(ERC721, ERC721URIStorage)
-        returns (string memory)
-    {
-        return super.tokenURI(tokenId);
-    }
-
-    function getIssuerInfo(address issuerWalletAddress)
-        external
-        view
-        returns (string memory contentHash)
-    {
-        if (!hasRole(ISSUER_ROLE, issuerWalletAddress))
-            revert IssuerNotFound(issuerWalletAddress);
-        return aboutIssuer[issuerWalletAddress];
+        return "USER";
     }
 
     /*============================================
     Overriding unnecessary inherited methods
     ============================================*/
-    function approve(address to, uint256 tokenId)
-        public
-        virtual
-        override(ERC721, IERC721)
-    {}
+    // function approve(address to, uint256 tokenId)
+    //     public
+    //     virtual
+    //     override(ERC721, IERC721)
+    // {}
 
-    function grantRole(bytes32 role, address account) public override {}
+    // function grantRole(bytes32 role, address account) public override {}
 
-    function revokeRole(bytes32 role, address account) public override {}
+    // function revokeRole(bytes32 role, address account) public override {}
 
-    function renounceRole(bytes32 role, address account) public override {}
+    // function renounceRole(bytes32 role, address account) public override {}
 
-    function safeTransferFrom(
-        address from,
-        address to,
-        uint256 tokenId
-    ) public virtual override(ERC721, IERC721) {}
+    // function safeTransferFrom(
+    //     address from,
+    //     address to,
+    //     uint256 tokenId
+    // ) public virtual override(ERC721, IERC721) {}
 
-    function safeTransferFrom(
-        address from,
-        address to,
-        uint256 tokenId,
-        bytes memory data
-    ) public virtual override(ERC721, IERC721) {}
+    // function safeTransferFrom(
+    //     address from,
+    //     address to,
+    //     uint256 tokenId,
+    //     bytes memory data
+    // ) public virtual override(ERC721, IERC721) {}
 
-    function setApprovalForAll(address operator, bool approved)
-        public
-        override(ERC721, IERC721)
-    {}
+    // function setApprovalForAll(address operator, bool approved)
+    //     public
+    //     override(ERC721, IERC721)
+    // {}
 
-    function transferFrom(
-        address from,
-        address to,
-        uint256 tokenId
-    ) public virtual override(ERC721, IERC721) {}
+    // function transferFrom(
+    //     address from,
+    //     address to,
+    //     uint256 tokenId
+    // ) public virtual override(ERC721, IERC721) {}
 
-    function getApproved(uint256 tokenId)
-        public
-        view
-        virtual
-        override(ERC721, IERC721)
-        returns (address)
-    {}
+    // function getApproved(uint256 tokenId)
+    //     public
+    //     view
+    //     virtual
+    //     override(ERC721, IERC721)
+    //     returns (address)
+    // {}
 
-    function getRoleAdmin(bytes32 role)
-        public
-        view
-        virtual
-        override
-        returns (bytes32)
-    {}
+    // function getRoleAdmin(bytes32 role)
+    //     public
+    //     view
+    //     virtual
+    //     override
+    //     returns (bytes32)
+    // {}
 
-    function hasRole(bytes32 role, address account)
-        public
-        view
-        virtual
-        override
-        returns (bool)
-    {}
+    // function hasRole(bytes32 role, address account)
+    //     public
+    //     view
+    //     virtual
+    //     override
+    //     returns (bool)
+    // {}
 
-    function isApprovedForAll(address owner, address operator)
-        public
-        view
-        virtual
-        override(ERC721, IERC721)
-        returns (bool)
-    {}
+    // function isApprovedForAll(address owner, address operator)
+    //     public
+    //     view
+    //     virtual
+    //     override(ERC721, IERC721)
+    //     returns (bool)
+    // {}
 }
--- scripts/deploy.js
+++ scripts/deploy.js
@@ -7,21 +7,11 @@
 const hre = require("hardhat");
 
 async function main() {
-  const currentTimestampInSeconds = Math.round(Date.now() / 1000);
-  const unlockTime = currentTimestampInSeconds + 60;
-
-  const lockedAmount = hre.ethers.parseEther("0.001");
-
-  const lock = await hre.ethers.deployContract("Lock", [unlockTime], {
-    value: lockedAmount,
-  });
-
-  await lock.waitForDeployment();
+  const SiddhiCred = await hre.ethers.deployContract("SiddhiCred");
+  await SiddhiCred.waitForDeployment();
 
   console.log(
-    `Lock with ${ethers.formatEther(
-      lockedAmount
-    )}ETH and unlock timestamp ${unlockTime} deployed to ${lock.target}`
+    `SiddhiCred contract deployed to ${SiddhiCred.target}`
   );
 }
 
