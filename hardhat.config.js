015e7fc881a3f3b37c8e3c9e56f4077a821d8f5a
jaimin1618 <jaimin.chokhawala@gmail.com>
2023-09-04 03:12:25 +0530

    hardhat optimizations for running contract more than limit size

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,27 @@
 require("@nomicfoundation/hardhat-toolbox");
 
 /** @type import('hardhat/config').HardhatUserConfig */
+// module.exports = {
+//   solidity: "0.8.18",
+// };
+
 module.exports = {
-  solidity: "0.8.18",
+  networks: {
+    localhost: {
+      allowUnlimitedContractSize: true,
+    },
+  },
+  solidity: {
+    compilers: [
+      {
+        version: "0.8.18",
+        settings: {
+          optimizer: {
+            enabled: true,
+            runs: 200,
+          },
+        },
+      },
+    ],
+  },
 };
